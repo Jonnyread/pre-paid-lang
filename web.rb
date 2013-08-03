5e0c83253ed599b63caaefba70801d3f11465e77
JonnyRead <jonnyread@yahoo.com>
2013-08-03 13:58:07 -0400

    test3

--- web.rb
+++ web.rb
@@ -27,6 +27,30 @@ get '/course/selected' do
     response = Twilio::TwiML::Response.new do |r|
       r.Say 'Welcome to course 1'
       r.Say 'This is where you would hear the course content.'
+      r.Gather :numDigits => '1', :action => '/course/selected/done', :method => 'get' do |g|
+        g.Say 'Press 1 to learn about Greeting the homeowner'
+        g.Say 'Press 2 to do course 2. How to negotiate a job.'
+        g.Say 'Press any other button to hear this menu again.'
+    end
+  elsif params['Digits'] == '2'
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Welcome to course 2'
+      r.Say 'This is where you would hear the course content.'
+    end
+  end
+  response.text
+end
+
+get '/course/selected/done' do
+  redirect '/course' unless ['1', '2'].include?(params['Digits'])
+  if params['Digits'] == '1'
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Welcome to course 1'
+      r.Say 'This is where you would hear the course content.'
+      r.Gather :numDigits => '1', :action => '/course/selected/done', :method => 'get' do |g|
+        g.Say 'Press 1 to learn about Greeting the homeowner'
+        g.Say 'Press 2 to do course 2. How to negotiate a job.'
+        g.Say 'Press any other button to hear this menu again.'
     end
   elsif params['Digits'] == '2'
     response = Twilio::TwiML::Response.new do |r|
