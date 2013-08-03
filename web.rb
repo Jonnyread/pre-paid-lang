c4f0d5d021660f41f960ed51fcb2e5a73bb1df67
JonnyRead <jonnyread@yahoo.com>
2013-08-03 14:10:39 -0400

    testagain

--- web.rb
+++ web.rb
@@ -27,36 +27,26 @@ get '/course/selected' do
     response = Twilio::TwiML::Response.new do |r|
       r.Say 'Welcome to course 1'
       r.Say 'This is where you would hear the course content.'
-      r.Gather :numDigits => '1', :action => '/course/selected/done', :method => 'get' do |g|
-        g.Say 'Press 1 to learn about Greeting the homeowner'
-        g.Say 'Press 2 to do course 2. How to negotiate a job.'
-        g.Say 'Press any other button to hear this menu again.'
     end
   elsif params['Digits'] == '2'
     response = Twilio::TwiML::Response.new do |r|
       r.Say 'Welcome to course 2'
       r.Say 'This is where you would hear the course content.'
+    end
+  elsif params['Digits'] == '3'
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Welcome to course 3'
+      r.Say 'This is where you would hear the course content.'
+      r.Gather :numDigits => '1', :action => '/course/test', :method => 'get' do |g|
+      	r.Say 'Would you like to try taking a pop quiz - press 1.'
     end
   end
   response.text
 end
 
-get '/course/selected/done' do
-  redirect '/course' unless ['1', '2'].include?(params['Digits'])
-  if params['Digits'] == '1'
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Welcome to course 1'
-      r.Say 'This is where you would hear the course content.'
-      r.Gather :numDigits => '1', :action => '/course/selected/done', :method => 'get' do |g|
-        g.Say 'Press 1 to learn about Greeting the homeowner'
-        g.Say 'Press 2 to do course 2. How to negotiate a job.'
-        g.Say 'Press any other button to hear this menu again.'
+get '/course/test' do
+  Twilio::TwiML::Response.new do |r|
+    r.Say "Welcome to the test"
     end
-  elsif params['Digits'] == '2'
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Welcome to course 2'
-      r.Say 'This is where you would hear the course content.'
-    end
-  end
-  response.text
+  end.text
 end
