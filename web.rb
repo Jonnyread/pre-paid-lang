65c96aa45a273976d7de3b9492bd5af4d51c4dfc
JonnyRead <jonnyread@yahoo.com>
2013-08-03 14:14:20 -0400

    tester

--- web.rb
+++ web.rb
@@ -12,7 +12,7 @@ end
 
 get '/course' do
   Twilio::TwiML::Response.new do |r|
-    r.Say "Welcome to pre paid language learning, the service that gets you fluent faster."
+    r.Say "Congratulations your code has been accepted.  Please select the course you'd like to take."
     r.Gather :numDigits => '1', :action => '/course/selected', :method => 'get' do |g|
       g.Say 'Press 1 to learn about Greeting the homeowner'
       g.Say 'Press 2 to do course 2. How to negotiate a job.'
@@ -22,7 +22,7 @@ get '/course' do
 end
 
 get '/course/selected' do
-  redirect '/course' unless ['1', '2'].include?(params['Digits'])
+  redirect '/course' unless ['1', '2', '3'].include?(params['Digits'])
   if params['Digits'] == '1'
     response = Twilio::TwiML::Response.new do |r|
       r.Say 'Welcome to course 1'
