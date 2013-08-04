0f7633c621decf8b41002dcfb1547c14c3d70263
JonnyRead <jonnyread@yahoo.com>
2013-08-04 10:24:37 -0400

    new test

--- web.rb
+++ web.rb
@@ -15,7 +15,8 @@ get '/course' do
     r.Say "Congratulations your code has been accepted.  Please select the course you'd like to take."
     r.Gather :numDigits => '1', :action => '/course/selected', :method => 'get' do |g|
       g.Say 'Press 1 to learn about Greeting the homeowner'
-      g.Say 'Press 2 to do course 2. How to negotiate a job.'
+      g.Say 'Press 2 to take course 2 how to negotiate a job.'
+      g.Say 'Press 3 to take course 3 how to ask a question about your job.'
       g.Say 'Press any other button to hear this menu again.'
     end
   end.text
@@ -25,18 +26,18 @@ get '/course/selected' do
   redirect '/course' unless ['1', '2', '3'].include?(params['Digits'])
   if params['Digits'] == '1'
     response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Welcome to course 1'
+      r.Say 'Welcome to course 1 greeting the homeowner'
       r.Say 'This is where you would hear the course content.'
     end
   elsif params['Digits'] == '2'
     response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Welcome to course 2'
+      r.Say 'Welcome to course 2 how to negotiate a job'
       r.Say 'This is where you would hear the course content.'
       r.Play 'https://s3.amazonaws.com/pre-paid-language/02+Dope+Boys+(Bird+Peterson+Remix).mp3'
     end
   elsif params['Digits'] == '3'
     response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Welcome to course 3'
+      r.Say 'Welcome to course 3 how to ask questions about your job'
       r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
     end
   end
