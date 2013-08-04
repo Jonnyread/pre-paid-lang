c796e8d2d8b9df61012926c724870231df27d098
JonnyRead <jonnyread@yahoo.com>
2013-08-04 10:45:12 -0400

    tester

--- web.rb
+++ web.rb
@@ -23,7 +23,7 @@ get '/course' do
 end
 
 get '/course/selected' do
-  redirect '/course' unless ['1', '2', '3'].include?(params['Digits'])
+  redirect '/course' unless ['1', '2', '3', '4'].include?(params['Digits'])
   if params['Digits'] == '1'
     response = Twilio::TwiML::Response.new do |r|
       r.Say 'Welcome to course 1 greeting the homeowner'
@@ -39,6 +39,10 @@ get '/course/selected' do
     response = Twilio::TwiML::Response.new do |r|
       r.Say 'Welcome to course 3 how to ask questions about your job'
       r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
+    end
+  elsif params['Digits'] == '4'
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Here is course number 4'
     end
   end
   response.text
