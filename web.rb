997e605b24c7bf4e48b6cbbfcbba2c22496da41d
JonnyRead <jonnyread@yahoo.com>
2013-08-11 18:53:12 -0400

    spanish test2

--- web.rb
+++ web.rb
@@ -14,7 +14,7 @@ get '/course' do
   Twilio::TwiML::Response.new do |r|
     r.Say "Congratulations your code has been accepted.  Please select the course you'd like to take."
     r.Gather :numDigits => '1', :action => '/course/selected', :method => 'get' do |g|
-      g.Say :voice => "woman", :language => "es" 'impulse el numero 1 para aprender mas sobre saludando el dueno'
+      g.Say 'impulse el numero 1 para aprender mas sobre saludando el dueno', :voice => "woman", :language => "es"
       g.Say 'Press 2 to take course 2 how to negotiate a job.'
       g.Say 'Press 3 to take course 3 how to ask a question about your job.'
       g.Say 'Press 4 to take course 4 how to negotiate your pay'
